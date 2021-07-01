e47ca3d445f937d345c87b05fc67ddd12d0918fe
Ben Hong <ben@bencodezen.io>
2021-07-01 13:35:40 -0400

    feature: update ooh 6

--- src/pages/SoloDisplayOOH.vue
+++ src/pages/SoloDisplayOOH.vue
@@ -7,8 +7,8 @@ export default {}
     <footer class="footer">
       <img src="/img/bencodezen-circle.png" alt="" class="footer-logo" />
       <div class="footer-text-wrapper">
-        <p class="footer-text-title">Obsidian Office Hours (#5)</p>
-        <p class="footer-text-subtitle">New Dataview Technique and more...</p>
+        <p class="footer-text-title">Obsidian Office Hours (#6)</p>
+        <p class="footer-text-subtitle">Scripting with Obsidian + Live Q&A</p>
       </div>
     </footer>
   </main>
