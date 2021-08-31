5cb7dc203bf9b88f09a18599caee9f7b69e8770b
Ben Hong <ben@bencodezen.io>
2021-08-31 11:36:06 -0400

    update bwb overlay

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,4 @@ body {
   margin: 0;
   font-family: 'Rubik', sans-serif;
 }
-
-.footer {
-  width: 100%;
-  height: 140px;
-  position: absolute;
-  bottom: 0;
-  background-color: rgb(6, 6, 96);
-  display: flex;
-  align-items: center;
-}
-
-.footer-logo {
-  max-width: 125px;
-  padding: 0 1rem;
-}
 </style>
--- src/pages/SoloDisplayBWB.vue
+++ src/pages/SoloDisplayBWB.vue
@@ -7,14 +7,15 @@ export default {}
     <footer class="footer">
       <img src="/img/dragon-avatar.png" alt="" class="footer-logo" />
       <div class="footer-text-wrapper">
-        <p class="footer-text-title">Build with Ben (#13)</p>
-        <p class="footer-text-subtitle">Nuxt + AirTable Adventures</p>
+        <p class="footer-text-title">
+          Build with Ben: Nuxt + AirTable Adventures (Part 2)
+        </p>
       </div>
     </footer>
   </main>
 </template>
 
-<style>
+<style scoped>
 .footer-text-wrapper {
   display: flex;
   flex-direction: column;
@@ -24,14 +25,22 @@ export default {}
 .footer-text-title {
   color: #fff;
   font-weight: bold;
-  font-size: 2rem;
-  margin-top: 15px;
-  margin-bottom: 0;
+  font-size: 1.5rem;
+  margin: 0;
 }
 
-.footer-text-subtitle {
-  font-weight: 400;
-  font-size: 1.2rem;
-  margin-top: 10px;
+.footer {
+  width: 100%;
+  height: 70px;
+  position: absolute;
+  bottom: 0;
+  background-color: rgb(6, 6, 96);
+  display: flex;
+  align-items: center;
+}
+
+.footer-logo {
+  max-width: 50px;
+  padding: 0 1rem;
 }
 </style>
