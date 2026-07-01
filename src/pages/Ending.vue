41a06bee9d3bf08ddeb80ebab4e9d702ac421077
Ben Hong <ben@bencodezen.io>
2026-07-01 11:58:11 -0400

    feat: add new ending scene

---
+++ src/pages/Ending.vue
@@ -0,0 +1,24 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <main class="starting">
+    <h1>Till next Wednesday at 12:30PM ET!</h1>
+  </main>
+</template>
+
+<style>
+.starting {
+  width: 1280px;
+  height: 720px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+}
+
+.starting h1 {
+  font-size: 3rem;
+}
+</style>
