eaa629a97d98d8e25222e62f57d16eb4afa06e0f
Ben Hong <ben@bencodezen.io>
2026-07-01 12:07:11 -0400

    feat: add ending scene route

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Starting from './pages/Starting.vue'
+import Ending from './pages/Ending.vue'
 import LiveScreen from './pages/LiveScreen.vue'
 
 const routes = [
   { path: '/starting', component: Starting },
+  { path: '/ending', component: Ending },
   { path: '/live-screen', component: LiveScreen }
 ]
 
